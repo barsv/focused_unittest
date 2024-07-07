9125ea9112521b785311f046de73b6b5c175a034
stanislav.baranov <barsv85@gmail.com>
2024-07-07 14:16:33 +0300

    trying to fix the build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='focused_unittest',
-    version='0.1.0',
+    version='0.1.1',
     description='Decorator to select only one unit test to be run.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
